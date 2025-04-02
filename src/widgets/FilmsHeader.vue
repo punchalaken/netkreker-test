f94384296e1b24166259c6ef37a02cb4bb622aad
Semen Kliuev <penekkakti@gmail.com>
2025-04-02 15:03:41 +0300

    Fix fiml__header classes

--- src/widgets/FilmsHeader.vue
+++ src/widgets/FilmsHeader.vue
@@ -4,7 +4,7 @@ import SortWidget from './SortWidget.vue'
 </script>
 
 <template>
-    <div class="films__header">
+    <div class="films__header__wrapper">
         <h1 class="films__header">Фильмы</h1>
         <SortWidget />
         <UnderlineWhite />
@@ -12,7 +12,8 @@ import SortWidget from './SortWidget.vue'
 </template>
 
 <style scoped lang="scss">
-.films__header {
+.films__header,
+.films__header__wrapper {
     @include flex-column;
     gap: 1em;
 }
